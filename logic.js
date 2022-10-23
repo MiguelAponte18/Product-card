ec1df9bd63bc718d2560132b71a8b474d26c5643
MiguelAponte18 <109483620+MiguelAponte18@users.noreply.github.com>
2022-10-22 21:17:30 -0400

    proyecto final

--- logic.js
+++ logic.js
@@ -1,15 +1,21 @@
 let boton = document.querySelector('.button'); 
+let cart = document.querySelector('.carrito');
 let notificacion = document.querySelector('.notificacion'); 
+let b = document.querySelector('.barra');
+let spa = document.querySelector('.text-span')
 
 boton.addEventListener('click', function escribir(){
  
     
-   notificacion.style.visibility = 'visible'
+   notificacion.style.visibility = 'visible';
  	notificacion.textContent = 1;
  
-
- 
-  console.log('tocado boton');
+   spa.textContent='Perfume 149.99$ added to cart :D';
       
 
-})
+});
+
+  cart.addEventListener('click', function hola(){
+     b.style.visibility=='hidden'? b.style.visibility='visible': b.style.visibility='hidden';
+
+  });
